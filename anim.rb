e8a4050e93c7046f0826ade0cb748d01942876d6
Jonas Heylen <jonas.heylen@gmail.com>
2008-12-28 19:34:47 +0100

    AnimatedGif class improvements

--- anim.rb
+++ anim.rb
@@ -12,12 +12,26 @@ class AnimatedGif
     @enddelay = enddelay
   end
   
-  def draw_frame
-    img = Image.new(@xsize, @ysize)
+  private
+  def draw_frame(img, func)
     draw = Draw.new
-    yield draw
+    func.call(draw)
     draw.draw(img)
     @anim << img.copy
+  end
+  
+  public
+  def draw_new_frame(&func)
+    draw_frame(Image.new(@xsize, @ysize), func)
+  end
+  
+  def draw_last_frame(&func)
+    begin
+      img = @anim.cur_image
+    rescue IndexError
+      img = Image.new(@xsize, @ysize)
+    end
+    draw_frame(img, func)
   end
   
   def write(filename)
@@ -38,7 +52,7 @@ end
 
 anim = AnimatedGif.new(100, 100, 100, 300)
 5.times do |i|
-  anim.draw_frame do |draw|
+  anim.draw_new_frame do |draw|
     draw.fill('transparent')
     draw.circle_radius_color(25, 25, 15 + i, 'red')
     draw.circle_radius_color(25, 75, 20 - i, 'blue')

b794815861878b7aa1bf4e71859203061facbf7c
Jonas Heylen <jonas.heylen@gmail.com>
2009-09-22 15:22:17 +0200

    Alfresco aliases

---
+++ alfaliases.sh
@@ -0,0 +1,7 @@
+export ALF=/opt/alfresco
+export EXT=$ALF/tomcat/shared/classes/alfresco/extension
+
+alias alfstart="cd $ALF; ./alfresco.sh start; cd -"
+alias alfstop="cd $ALF; ./alfresco.sh stop; cd -"
+alias psgj="ps aux | grep java"
+

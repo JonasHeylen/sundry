1c8fac6119dd2896f0d469977a2c36f11895f839
Jonas Heylen <jonas.heylen@gmail.com>
2009-09-24 09:56:36 +0200

    Added alias to tail logfile

--- alfaliases.sh
+++ alfaliases.sh
@@ -3,5 +3,6 @@ export EXT=$ALF/tomcat/shared/classes/alfresco/extension
 
 alias alfstart="cd $ALF; ./alfresco.sh start; cd -"
 alias alfstop="cd $ALF; ./alfresco.sh stop; cd -"
+alias alftail="tail -f $ALF/tomcat/logs/catalina.out"
 alias psgj="ps aux | grep java"
 
